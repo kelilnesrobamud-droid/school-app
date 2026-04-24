5b6ff059a9160a30b8b62c1d179b263d558210c8
Keliln <kelil.nesro.bamud@gmail.com>
2026-04-24 19:04:17 +0300

    Add Firebase configuration and initialization

--- config.js
+++ config.js
@@ -1,1 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "የአንተ_API_KEY",
+  authDomain: "ፕሮጀክትህ.firebaseapp.com",
+  projectId: "ፕሮጀክትህ-ID",
+  storageBucket: "ፕሮጀክትህ.appspot.com",
+  messagingSenderId: "ቁጥር",
+  appId: "APP-ID"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
 
