31985f004622e38cd50145cb41615c98d8a69d6a
Keliln <kelil.nesro.bamud@gmail.com>
2026-04-25 18:30:13 +0300

    Add Firebase configuration and initialization

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBYfvuRAupj4_bDeaSJRMtpz_lohdd1XHM", // እዚህ ጋ H መሆኗን አረጋግጥ
+  authDomain: "school-app-945a1.firebaseapp.com",
+  databaseURL: "https://school-app-945a1-default-rtdb.firebaseio.com",
+  projectId: "school-app-945a1",
+  storageBucket: "school-app-945a1.firebasestorage.app",
+  messagingSenderId: "1074529168380",
+  appId: "1:1074529168380:web:eb23f0ee65dd7770bca079",
+  measurementId: "G-E191ECKYG8"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export default app;
 
