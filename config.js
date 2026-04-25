e050c8dfe939e5b98bfe63b8eb3aa330fe0cae6a
Keliln <kelil.nesro.bamud@gmail.com>
2026-04-25 08:56:10 +0300

    Remove Firebase initialization code from config.js

--- config.js
+++ config.js
@@ -1,17 +1,1 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-
-const firebaseConfig = {
-  apiKey: "የአንተ_API_KEY",
-  authDomain: "ፕሮጀክትህ.firebaseapp.com",
-  projectId: "ፕሮጀክትህ-ID",
-  storageBucket: "ፕሮጀክትህ.appspot.com",
-  messagingSenderId: "ቁጥር",
-  appId: "APP-ID"
-};
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
 
